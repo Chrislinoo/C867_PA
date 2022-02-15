15106e8eb72b9f023077896130f5f8a88341f1bb
Christopher <chrislinoo@icloud.com>
2022-02-14 20:14:16 -0500

    Add project files.

---
+++ C867_PA/degree.h
@@ -0,0 +1,2 @@
+#pragma once
+enum DegreeProgram {SECURITY,NETWORK,SOFTWARE}; /*enum allows you to enumerate data values and inside the curly brackets are the data type values unless specifically specified they are valued in ascending order from left to right 0 through +infinity*/
---
+++ C867_PA/student.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "degree.h"
+
