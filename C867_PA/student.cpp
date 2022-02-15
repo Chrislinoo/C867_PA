e6f280a64ce355dd9b68b4cd4c16d28839bf9c55
Christopher <chrislinoo@icloud.com>
2022-02-15 17:42:51 -0500

    Worked on the classes and started defining and next should be setters and getters

--- C867_PA/student.cpp
+++ C867_PA/student.cpp
@@ -2,3 +2,27 @@
 #include <string>
 #include "student.h"
 using namespace std;
+
+Student::Student()
+{
+	this->studentID = "";
+	this->firstName = "";
+	this->lastName = "";
+	this->email = "";
+	this->age = 0;
+	for (int i = 0; i < dayArray; i++) this->daysLeft[i] = 0;
+	this->degreeProgram = DegreeProgram::SOFTWARE;
+}
+
+Student::Student(string studentID, string firstName, string lastName, string email, int age, int daysLeft[], DegreeProgram degreeProgram)
+{
+	this->studentID = studentID; this->firstName = firstName; this->lastName = lastName; this->email = email;
+	this->age = age;
+	for (int i = 0; i < dayArray; i++;) this->daysLeft[i] = daysLeft[i];
+	this->degreeProgram = degreeProgram;
+}
+
+Student::~Student()
+{
+	//think i leave it in blank bc it does nothing
+}
--- C867_PA/student.h
+++ C867_PA/student.h
@@ -1,3 +1,28 @@
 #pragma once
 #include "degree.h"
 
+class Student {
+	/*		constructor and destructor		*/
+public:
+	//Student();//created the definition in student.cpp
+	Student(string studentID, string firstName, string lastName, string email, int age, int daysLeft[], DegreeProgram degreeProgram);
+	~Student();
+
+	const static int dayArray = 3;//needed to define the array
+
+	/*		Getters and Setters		*/
+
+	//setters
+
+	string setID(string);
+
+
+private:
+	string studentID;
+	string firstName;
+	string lastName;
+	string email;
+	int age;
+	int daysLeft[dayArray];
+	DegreeProgram degreeProgram;
+};
